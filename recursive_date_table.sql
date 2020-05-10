dc3b1d36842e3baf53b306d6f0ceb5ed0e64176a
Philip Lancaster <33505124+plancaster88@users.noreply.github.com>
2020-05-09 21:25:46 -0500

    Update recursive_date_table.sql

--- recursive_date_table.sql
+++ recursive_date_table.sql
@@ -13,8 +13,11 @@ declare @ed datetime = '2025-12-31' --End date here
     where [Date] < @ed
 )
 
+
+create view [pe].[vDates] as 
+
 select 
-	[Date]
+	  [Date]
 	, [Year] = Year([date]) 
 	, [Month] = Month([date])
 	, [MonthName] = datename(mm, [date])
@@ -22,28 +25,34 @@ select
 	, [Quarter] = datename(qq, [date]) 
 	, QuarterName = case	when datename(qq, [date]) = 1 then 'First' when datename(qq, [date]) = 2 then 'Second' 
 							when datename(qq, [date]) = 3 then 'Third' when datename(qq, [date]) = 4 then 'Fourth' end
+	, QQ = concat('Q', datename(qq, [date]))
 	, [Day] = datepart(dy, [date]) 
 	, [DayOfWeek] = datepart(dw, [date])	
 	, [DayOfMonth] = Day([date])
 	, [DayName] = datename(w, [date])
 	, DayNameShort = LEFT(datename(w, [date]),2)
 	, [Week] = datepart(wk, [date])
-	, YYYYMM = format([date], 'yyyyMM') --convert(varchar(6), date, 112)
-	, MMMYY = format([date], 'MMM-yy') 
-	, YYYYQ = concat(format([date], 'yyyy'), datename(q, [date]))
-	, QQYY = concat('Q',datename(q, [date]),'-', format([date], 'yy'))
+	, WeekOfMonth = (DATEPART(week, [date]) - DATEPART(week, DATEADD(day, 1, EOMONTH([date], -1)))) + 1
 	, StartOfWeek =  dateadd(d, -( datepart(dw, [date]) - 1), [date])
 	, EndOfWeek = dateadd(d, 7 -  datepart(dw, [date]), [date])
-	, StartOfMonth = DATEADD(mm, DATEDIFF(mm,0,[date]), 0)
-	, EndOfMonth = cast(eomonth([date]) as datetime)
+	, StartOfMonth = DATEFROMPARTS(Year([date]), Month([date]), 1)
+	, EndOfMonth = cast(eomonth([date]) as date) 
+	, StartOfYear = DATEFROMPARTS(YEAR([date]), 1, 1)
+	, LastOfYear   = DATEFROMPARTS(YEAR([date]), 12, 31)
+	, YYYYMM = format([date], 'yyyyMM') --convert(varchar(6), date, 112)
+	, YYYYMMDD = format([date], 'yyyyMMdd') --convert(varchar(6), date, 112)
+	, MMMYY = format([date], 'MMM-yy') 
+	, YYYYQQ = concat(format([date], 'yyyy'),'Q', datename(q, [date]))
+	, QQYY = concat('Q',datename(q, [date]),'-', format([date], 'yy'))
 	, IsCurrentYear = case when year([date]) = year(getdate()) then 1 else 0 end 
 	, IsLastYear = case when year([date]) = year(getdate()) - 1 then 1 else 0 end 
+	, IsFutureYear = case when year([date]) > year(getdate()) then 1 else 0 end 
 	, IsCurrentMonth = case when year([date]) = year(getdate()) and month(date) = month(getdate()) then 1 else 0 end 
-	, IsFutureMonth = case when CONVERT(varchar(6), GETDATE(), 112) < format([date], 'yyyyMM') then 1 else 0 end 
 	, IsLastMonth = case when cast(eomonth([date]) as datetime) = eomonth(dateadd(m,-1, getdate())) then 1 else 0 end 
-	, IsCurrentDate = case when [date] = getdate() then 1 else 0 end 
-	, IsLagPeriod = case when DATEADD(mm, DATEDIFF(mm,0,[date]), 0) < dateadd(m, -3, getdate()) then 1 else 0 end 
-	, LagPeriodYoY = 
+	, IsFutureMonth = case when CONVERT(varchar(6), GETDATE(), 112) < format([date], 'yyyyMM') then 1 else 0 end 
+	, IsCurrentDate = case when [date] = cast(getdate() as date) then 1 else 0 end 
+	, IsClaimLagPeriod = case when DATEADD(mm, DATEDIFF(mm,0,[date]), 0) < dateadd(m, -3, getdate()) then 1 else 0 end 
+	, IsClaimLagPeriodYoY = 
 		case	when month(getdate()) between 1 and 3 
 					and Month([date]) <= month(dateadd(m, -3, getdate()))
 					and Year([date])  <> year(getdate())	then 1 
@@ -52,6 +61,7 @@ select
 				else 0 end   			 
 	, IsLeapYear = CASE WHEN (year([date]) % 4 = 0 AND year([date]) % 100 <> 0) OR year([date]) % 400 = 0 THEN 1 else 0 end 
 from cte_dates
+
 
 option (maxrecursion 32767) 
 
